320e47c93f02ce0521572be8fd3259adfa2fd248
Vignesh-D18 <2023cs_vigneshd_d@nie.ac.in>
2025-09-18 09:35:25 +0530

    Initial commit - Ecom_App project

---
+++ Ecom_App/src/main/java/com/nie/csd/exceptions/IdNotPresentException.java
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+package com.nie.csd.exceptions;
+
+public class IdNotPresentException extends Exception {
+    public IdNotPresentException(String message) {
+        super(message);
+    }
+    
+}
+=======
+package com.nie.csd.exceptions;
+
+public class IdNotPresentException extends Exception {
+    public IdNotPresentException(String message) {
+        super(message);
+    }
+    
+}
+>>>>>>> f619afa (Initial commit)
